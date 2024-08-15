2191d1d0fd6bb7d8af1463c77deabd2b0173814d
david0405166 <davidordonio92@gmail.com>
2024-08-15 23:15:10 +0800

    add the the correct binding

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  //Ensure the main.ts file has the correct binding
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
